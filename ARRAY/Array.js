43b3a27b3efd56b9411fb83606ae5042c8e1ac6c
REBUG2416 <davidagochukwu@gmail.com>
2024-01-13 13:49:31 +0100

    NEW

--- ARRAY/Array.js
+++ ARRAY/Array.js
@@ -7,3 +7,70 @@ function arrfil(arr){
     if(nam.includes(arr))
    return arr;
 }
+
+
+let myArr=["Hey","Hello","Hi"]
+
+myArr.push("Help")
+
+console.log(myArr)
+
+console.log(myArr.shift())
+
+console.log(myArr)
+
+console.log(myArr.unshift("Hey","Help"))
+
+console.log(myArr)
+
+myArr.splice(2,2, "Help","Help");
+
+console.log(myArr)
+
+let mergeArr = myArr.concat(arr);
+
+console.log(mergeArr)
+
+var mistake = myArr.find((e) => e !== "Help")
+
+console.log(mistake)
+
+mergeArr.sort();
+
+console.log(mergeArr)
+
+let num=[6,2,6];
+
+const sum = num.reduce((accumulator, currentValue, indexOfCurrentValue, totalArrayElement) => accumulator + currentValue, 4);
+console.log(sum)
+
+const evenNumbers = num.filter(function(num) {
+    return num !== 6;
+  });
+  
+  console.log(evenNumbers); 
+
+  let numArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+  let newElements = numArr.filter((e) => e <10 )
+  console.log(newElements)
+
+  newElements.sort((a,b) => a-b)
+
+
+  let nameArr= ["David","Bryan", "Daniel" , "Alice"]
+
+ console.log(nameArr.includes("Alice"))
+
+console.log(nameArr.reduce((accumulator, currentValue, indexOfCurrentValue, totalArrayElement) => accumulator + currentValue.length, 0))
+
+
+bookarr=[book1={title: "Javascript", Author: "IDK"},book2={title: "Harry Poter", Author: "J.K Rowling"},book3={title: "Harry Poter II", Author: "J.K Rowling"}]
+
+console.log(JKarr = bookarr.filter((e)=> (e.Author === "J.K Rowling") ))
+
+console.log(bookarr.find((e)=> e.title ==="Javascript"))
+
+console.log()
+
+ 
